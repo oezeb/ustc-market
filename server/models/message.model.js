c98d4ed1e4878167dcaa80bd444f1232d9cef191
oezeb <eouedraogo4@gmail.com>
2023-10-04 15:45:01 +0800

    add referenced item field to user model

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const messageSchema = new mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    item: { type: mongoose.Schema.Types.ObjectId, ref: 'Item' },
     content: { type: String },
 }, { timestamps: true });
 

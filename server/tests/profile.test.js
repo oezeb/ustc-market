87f78dec27e9eca3ac2c88850cd0d986724f1641
oezeb <eouedraogo4@gmail.com>
2023-10-05 06:07:00 +0800

    Update profile.test.js

--- server/tests/profile.test.js
+++ server/tests/profile.test.js
@@ -4,26 +4,42 @@ const bcrypt = require('bcryptjs')
 const app = require("../app");
 const User = require("../models/user.model");
 const Item = require("../models/item.model");
+const Message = require("../models/message.model");
 
 require("dotenv").config({ path: "./tests/.env" });
 
-user = {
+const login = async (user) => {
+    const response = await request(app)
+        .post("/api/login")
+        .auth(user.username, user.password);
+    expect(response.status).toBe(200);
+    return response.headers['set-cookie'];
+};
+
+const user = {
     username: "test",
     password: "test"
 }
+var cookie;
 
 beforeAll(async () => {
     await mongoose.connect(process.env.MONGODB_URI);
-    await new User({ 
-        username: "test", 
-        password: await bcrypt.hash("test", 10) 
+    const model = await new User({
+        username: user.username,
+        password: await bcrypt.hash(user.password, 10)
     }).save();
+    user._id = model._id;
+    cookie = await login(user);
 });
   
 afterAll(async () => {
     await User.deleteMany({});
     await mongoose.connection.close();
 });
+
+// =============================================================================
+// Profile
+// =============================================================================
 
 describe("GET /api/profile", () => {
     it("should return 401 if not logged in", async () => {
@@ -33,12 +49,6 @@ describe("GET /api/profile", () => {
 
     it("should return 200 if logged in", async () => {
         let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
-        response = await request(app)
             .get("/api/profile")
             .set('Cookie', cookie);
         expect(response.status).toBe(200);
@@ -53,29 +63,38 @@ describe("PATCH /api/profile", () => {
     });
 
     it("should return 204 if logged in", async () => {
-        let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
         const newpassword = "newpassword";
-        response = await request(app)
+        let response = await request(app)
             .patch("/api/profile")
             .set('Cookie', cookie)
             .send({ password: newpassword });
         expect(response.status).toBe(204);
 
-        response = await request(app)
-            .post("/api/login")
-            .auth(user.username, newpassword);
-        expect(response.status).toBe(200);
-
         user.password = newpassword;
+        cookie = await login(user);
     });
 });
 
+// =============================================================================
+// Profile Items
+// =============================================================================
+
 describe("GET /api/profile/items", () => {
+    var items = [];
+
+    beforeAll(async () => {
+        for (let i = 0; i < 3; i++) {
+            items.push(
+                await new Item({
+                    owner: user._id,
+                    name: `test${i}`
+                }).save()
+            );
+        }
+    });
+
+    afterAll(async () => { await Item.deleteMany({}); });
+
     it("should return 401 if not logged in", async () => {
         const response = await request(app).get("/api/profile/items");
         expect(response.status).toBe(401);
@@ -83,30 +102,10 @@ describe("GET /api/profile/items", () => {
 
     it("should return 200 if logged in", async () => {
         let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
-        response = await request(app)
             .get("/api/profile/items")
             .set('Cookie', cookie);
         expect(response.status).toBe(200);
-        expect(response.body.length).toBe(0);
-
-        const owner = await User.findOne({ username: user.username });
-        for (let i = 0; i < 3; i++) {
-            await new Item({ 
-                owner: owner._id,
-                name: `test${i}` 
-            }).save();
-        }
-
-        response = await request(app)
-            .get("/api/profile/items")
-            .set('Cookie', cookie);
-        expect(response.status).toBe(200);
-        expect(response.body.length).toBe(3);
+        expect(response.body.length).toBe(items.length);
 
         response = await request(app)
             .get("/api/profile/items")
@@ -114,20 +113,20 @@ describe("GET /api/profile/items", () => {
             .set('Cookie', cookie);
         expect(response.status).toBe(200);
         expect(response.body.length).toBe(1);
-        expect(response.body[0].name).toBe("test1");
+        expect(response.body[0].name).toBe(items[1].name);
 
         const itemId = response.body[0]._id;
         response = await request(app)
             .get(`/api/profile/items/${itemId}`)
             .set('Cookie', cookie);
         expect(response.status).toBe(200);
-        expect(response.body.name).toBe("test1");
+        expect(response.body.name).toBe(items[1].name);
     });
-
-    afterAll(async () => { await Item.deleteMany({}); });
 });
 
 describe("POST /api/profile/items", () => {
+    afterAll(async () => { await Item.deleteMany({}); });
+
     it("should return 401 if not logged in", async () => {
         const response = await request(app).post("/api/profile/items");
         expect(response.status).toBe(401);
@@ -135,12 +134,6 @@ describe("POST /api/profile/items", () => {
 
     it("should return 201 if logged in", async () => {
         let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
-        response = await request(app)
             .post("/api/profile/items")
             .set('Cookie', cookie)
             .send({ name: "test" });
@@ -148,73 +141,65 @@ describe("POST /api/profile/items", () => {
         expect(response.body.name).toBe("test");
 
         const item = await Item.findOne({ name: "test" });
-        const owner = await User.findOne({ username: user.username });
         expect(item).not.toBeNull();
         expect(item._id.toString()).toBe(response.body._id);
-        expect(item.owner.toString()).toBe(owner._id.toString());
+        expect(item.owner.toString()).toBe(user._id.toString());
     });
-
-    afterAll(async () => { await Item.deleteMany({}); });
 });
 
 describe("PATCH /api/profile/items/:id", () => {
+    var itemId;
+
+    beforeAll(async () => {
+        const item = await new Item({
+            owner: user._id,
+            name: "test"
+        }).save();
+        itemId = item._id;
+    });
+
+    afterAll(async () => { await Item.deleteMany({}); });
+
     it("should return 401 if not logged in", async () => {
-        const response = await request(app).patch("/api/profile/items/1");
+        const response = await request(app).patch(`/api/profile/items/${itemId}`);
         expect(response.status).toBe(401);
     });
 
     it("should return 204 if logged in", async () => {
         let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
-        const owner = await User.findOne({ username: user.username });
-        const item = await new Item({ 
-            owner: owner._id,
-            name: "test" 
-        }).save();
-
-        response = await request(app)
-            .patch(`/api/profile/items/${item._id}`)
+            .patch(`/api/profile/items/${itemId}`)
             .set('Cookie', cookie)
             .send({ name: "test2", tags: ["test"] });
         expect(response.status).toBe(204);
 
-        const updatedItem = await Item.findOne({ _id: item._id });
+        const updatedItem = await Item.findOne({ _id: itemId });
         expect(updatedItem.name).toBe("test2");
         expect(updatedItem.tags).toEqual(["test"]);
     });
-
-    afterAll(async () => { await Item.deleteMany({}); });
 });
 
 describe("DELETE /api/profile/items/:id", () => {
+    var itemId;
+    beforeAll(async () => {
+        const item = await new Item({
+            owner: user._id,
+            name: "test"
+        }).save();
+        itemId = item._id;
+    });
+
     it("should return 401 if not logged in", async () => {
-        const response = await request(app).delete("/api/profile/items/1");
+        const response = await request(app).delete(`/api/profile/items/${itemId}`);
         expect(response.status).toBe(401);
     });
 
     it("should return 204 if logged in", async () => {
         let response = await request(app)
-            .post("/api/login")
-            .auth(user.username, user.password);
-        expect(response.status).toBe(200);
-
-        const cookie = response.headers['set-cookie'];
-        const owner = await User.findOne({ username: user.username });
-        const item = await new Item({ 
-            owner: owner._id,
-            name: "test" 
-        }).save();
-
-        response = await request(app)
-            .delete(`/api/profile/items/${item._id}`)
+            .delete(`/api/profile/items/${itemId}`)
             .set('Cookie', cookie);
         expect(response.status).toBe(204);
 
-        const deletedItem = await Item.findOne({ _id: item._id });
+        const deletedItem = await Item.findOne({ _id: itemId });
         expect(deletedItem).toBeNull();
     });
 });

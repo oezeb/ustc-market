1c97dfebdecfc8534ffa118c2c2b36de36a1709f
oezeb <eouedraogo4@gmail.com>
2023-10-24 16:40:13 +0800

    add more api routes register, send verification email, verify email, send reset password email, reset password,

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,12 @@
 export const apiRoutes = {
     // auth
+    register: "/api/auth/register",
     login: "/api/auth/login",
     logout: "/api/auth/logout",
+    sendVerificationEmail: "/api/auth/send-verification-email",
+    verifyEmail: "/api/auth/verify-email",
+    sendResetPasswordEmail: "/api/auth/send-reset-password-email",
+    resetPassword: "/api/auth/reset-password",
 
     // items
     items: "/api/items",
